6ae512abcd29d20ca1230e498a790afcf9586f76
unknown <chemmasterynahida@gmail.com>
2025-10-18 23:17:36 +0900

    photoelectric effect

--- src/components/StudyPanel.svelte
+++ src/components/StudyPanel.svelte
@@ -14,7 +14,7 @@ tags = params.has("tag") ? params.getAll("tag") : [];
 categories = params.has("category") ? params.getAll("category") : [];
 const uncategorized = params.get("uncategorized");
 
-let allowedCategories = ["양자화학", "물리화학", "계산화학", "양자컴퓨터"];
+let allowedCategories = ["Quantum Chemistry", "Physical Chemistry", "Computational Chemistry", "Quantum Computing"];
 
 interface Post {
 	slug: string;
@@ -82,7 +82,9 @@ onMount(async () => {
 
 	const groupedPostsArray = Object.keys(grouped).map((category) => ({
 		category,
-		posts: grouped[category],
+		posts: grouped[category].sort((a, b) =>
+		a.data.title.localeCompare(b.data.title),
+	),
 	}));
 
 	groupedPostsArray.sort((a, b) => a.category.localeCompare(b.category));
