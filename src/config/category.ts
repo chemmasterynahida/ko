3ca30624471bc14428d2d6c851d897ac8b5ef92e
chemmasterynahida <chemmasterynahida@gmail.com>
2026-06-09 02:35:53 +0900

    renewal

---
+++ src/config/category.ts
@@ -0,0 +1,8 @@
+export const CATEGORY_IMAGE_MAP: Record<string, string> = {
+  "양자화학": "https://gcore.jsdelivr.net/gh/chemmasterynahida/assets/images/nahida-wp-66-1.jpg",
+  "물리화학": "https://gcore.jsdelivr.net/gh/chemmasterynahida/assets/images/nahida-wp-66-2.jpg",
+  "계산화학": "https://gcore.jsdelivr.net/gh/chemmasterynahida/assets/images/nahida-wp.png"
+};
+
+export const DEFAULT_CATEGORY_IMAGE =
+  "https://gcore.jsdelivr.net/gh/chemmasterynahida/assets/images/nahida.png";
